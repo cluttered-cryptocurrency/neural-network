9adcaec90720a96aced5ae61e9a8ef29e7629d1b
David Clutter <cluttered.code@gmail.com>
2017-07-11 22:44:25 -0600

    Add NeuronBuilder

---
+++ src/main/java/com/cluttered/cryptocurrency/ann/neuron/NeuronBuilder.java
@@ -0,0 +1,64 @@
+package com.cluttered.cryptocurrency.ann.neuron;
+
+import ch.obermuhlner.math.big.BigFloat;
+import com.cluttered.cryptocurrency.ann.activation.Activation;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static com.cluttered.cryptocurrency.BigFloatConstants.ONE;
+import static com.cluttered.cryptocurrency.ann.activation.Activation.LINEAR;
+import static com.cluttered.cryptocurrency.ann.activation.Activation.SIGMOID;
+import static com.cluttered.cryptocurrency.ann.activation.Activation.TAN_H;
+
+/**
+ * @author cluttered.code@gmail.com
+ */
+class NeuronBuilder {
+
+    private BigFloat bias = ONE;
+    private List<BigFloat> weights = Collections.singletonList(ONE);
+    private Activation activation = LINEAR;
+
+    private NeuronBuilder() {}
+
+    static NeuronBuilder create() {
+        return new NeuronBuilder();
+    }
+
+    public NeuronBuilder bias(final BigFloat bias) {
+        this.bias = bias;
+        return this;
+    }
+
+    public NeuronBuilder weights(final List<BigFloat> weights) {
+        this.weights = weights;
+        return this;
+    }
+
+    public NeuronBuilder weights(final BigFloat... weights) {
+        return weights(Arrays.asList(weights));
+    }
+
+    public NeuronBuilder activation(final Activation activation) {
+        this.activation = activation;
+        return this;
+    }
+
+    public NeuronBuilder linear() {
+        return activation(LINEAR);
+    }
+
+    public NeuronBuilder sigmoid() {
+        return activation(SIGMOID);
+    }
+
+    public NeuronBuilder tanH() {
+        return activation(TAN_H);
+    }
+
+    public Neuron build() {
+        return new Neuron(bias, weights, activation);
+    }
+}
---
+++ src/test/java/com/cluttered/cryptocurrency/ann/neuron/NeuronBuilderTest.java
@@ -0,0 +1,101 @@
+package com.cluttered.cryptocurrency.ann.neuron;
+
+import ch.obermuhlner.math.big.BigFloat;
+import com.cluttered.cryptocurrency.ann.activation.Activation;
+import mockit.Deencapsulation;
+import mockit.Mocked;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+import static com.cluttered.cryptocurrency.BigFloatConstants.ONE;
+import static com.cluttered.cryptocurrency.ann.activation.Activation.LINEAR;
+import static com.cluttered.cryptocurrency.ann.activation.Activation.SIGMOID;
+import static com.cluttered.cryptocurrency.ann.activation.Activation.TAN_H;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author cluttered.code@gmail.com
+ */
+public class NeuronBuilderTest {
+
+    @Test
+    public void testCreate() {
+        final NeuronBuilder neuronBuilder = NeuronBuilder.create();
+
+        final BigFloat privateBias = Deencapsulation.getField(neuronBuilder, "bias");
+        final List<BigFloat> privateWeights = Deencapsulation.getField(neuronBuilder, "weights");
+        final Activation privateActivation = Deencapsulation.getField(neuronBuilder, "activation");
+
+        assertThat(privateBias).isEqualTo(ONE);
+        assertThat(privateWeights).isEqualTo(Collections.singletonList(ONE));
+        assertThat(privateActivation).isEqualTo(LINEAR);
+    }
+
+    @Test
+    public void testBias(@Mocked final BigFloat bias) {
+        final NeuronBuilder neuronBuilder = NeuronBuilder.create().bias(bias);
+
+        final BigFloat privateBias = Deencapsulation.getField(neuronBuilder, "bias");
+
+        assertThat(privateBias).isEqualTo(bias);
+    }
+
+    @Test
+    public void testWeights(@Mocked final BigFloat weight1,
+                            @Mocked final BigFloat weight2,
+                            @Mocked final BigFloat weight3) {
+        final NeuronBuilder neuronBuilder = NeuronBuilder.create().weights(weight1, weight2, weight3);
+
+        final List<BigFloat> privateWeights = Deencapsulation.getField(neuronBuilder, "weights");
+
+        assertThat(privateWeights).containsExactly(weight1, weight2, weight3);
+    }
+
+    @Test
+    public void testLinear() {
+        final NeuronBuilder neuronBuilder = NeuronBuilder.create().linear();
+
+        final Activation privateActivation = Deencapsulation.getField(neuronBuilder, "activation");
+
+        assertThat(privateActivation).isEqualTo(LINEAR);
+    }
+
+    @Test
+    public void testSigmoid() {
+        final NeuronBuilder neuronBuilder = NeuronBuilder.create().sigmoid();
+
+        final Activation privateActivation = Deencapsulation.getField(neuronBuilder, "activation");
+
+        assertThat(privateActivation).isEqualTo(SIGMOID);
+    }
+
+    @Test
+    public void testHyperbolicTangent() {
+        final NeuronBuilder neuronBuilder = NeuronBuilder.create().tanH();
+
+        final Activation privateActivation = Deencapsulation.getField(neuronBuilder, "activation");
+
+        assertThat(privateActivation).isEqualTo(TAN_H);
+    }
+
+    @Test
+    public void testBuild(@Mocked final BigFloat bias,
+                          @Mocked final List<BigFloat> weights,
+                          @Mocked final Activation activation) {
+        final Neuron neuron = NeuronBuilder.create()
+                .bias(bias)
+                .weights(weights)
+                .activation(activation)
+                .build();
+
+        final BigFloat privateBias = Deencapsulation.getField(neuron, "bias");
+        final List<BigFloat> privateWeights = Deencapsulation.getField(neuron, "weights");
+        final Activation privateActivation = Deencapsulation.getField(neuron, "activation");
+
+        assertThat(privateBias).isEqualTo(bias);
+        assertThat(privateWeights).isEqualTo(weights);
+        assertThat(privateActivation).isEqualTo(activation);
+    }
+}
