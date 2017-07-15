ae999f24a05ef6f2df6b9fce4d82ff6a6d6ed334
David Clutter <cluttered.code@gmail.com>
2017-07-15 12:12:46 -0600

    create LayerTest boilerplate

---
+++ src/test/java/com/cluttered/cryptocurrency/ann/LayerTest.java
@@ -0,0 +1,32 @@
+package com.cluttered.cryptocurrency.ann;
+
+import com.cluttered.cryptocurrency.ann.neuron.Neuron;
+import mockit.Injectable;
+import mockit.Tested;
+import mockit.integration.junit4.JMockit;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author cluttered.code@gmail.com
+ */
+@RunWith(JMockit.class)
+public class LayerTest {
+
+    @Tested
+    @SuppressWarnings("unused")
+    private Layer layer;
+
+    @Injectable
+    @SuppressWarnings("unused")
+    private List<Neuron> neurons;
+
+    @Test
+    public void testFire() {
+        assertThat(true).isTrue();
+    }
+}
