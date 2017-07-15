89c056d08c1e69f905a9d5d88a5e7bbcd7939230
David Clutter <cluttered.code@gmail.com>
2017-07-15 10:39:09 -0600

    finish writing NeuralNetworkTest

--- src/test/java/com/cluttered/cryptocurrency/ann/NeuralNetworkTest.java
+++ src/test/java/com/cluttered/cryptocurrency/ann/NeuralNetworkTest.java
@@ -1,13 +1,19 @@
 package com.cluttered.cryptocurrency.ann;
 
+import ch.obermuhlner.math.big.BigFloat;
+import mockit.Expectations;
 import mockit.Injectable;
+import mockit.Mocked;
 import mockit.Tested;
 import mockit.integration.junit4.JMockit;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
+import static mockit.Deencapsulation.setField;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -29,7 +35,25 @@ public class NeuralNetworkTest {
     private List<Layer> layers;
 
     @Test
-    public void testFire() {
-        assertThat(true).isTrue();
+    public void testFire(@Mocked List<BigFloat> inputs,
+                         @Mocked Layer layer,
+                         @Mocked BigFloat expected1,
+                         @Mocked BigFloat expected2,
+                         @Mocked BigFloat expected3) {
+        final List<Layer> layers = Arrays.asList(layer, layer, layer);
+        setField(neuralNetwork, "layers", layers);
+        final List<BigFloat> expectedList1 = Collections.singletonList(expected1);
+        final List<BigFloat> expectedList2 = Collections.singletonList(expected2);
+        final List<BigFloat> expectedList3 = Collections.singletonList(expected3);
+
+        new Expectations() {{
+            layer.fire(inputs); times = 1; result = expected1;
+            layer.fire(expectedList1); times = 1; result = expected2;
+            layer.fire(expectedList2); times = 1; result = expected3;
+        }};
+
+        final List<BigFloat> result = neuralNetwork.fire(inputs);
+
+        assertThat(result).isEqualTo(expectedList3);
     }
 }
