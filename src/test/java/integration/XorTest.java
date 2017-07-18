30ca4fe1b871c0bc362b7e347a57e005dc54af3c
David Clutter <cluttered.code@gmail.com>
2017-07-17 20:39:43 -0600

    comment out logging in XorTest

--- src/test/java/integration/XorTest.java
+++ src/test/java/integration/XorTest.java
@@ -4,8 +4,6 @@ import com.cluttered.cryptocurrency.ann.Layer;
 import com.cluttered.cryptocurrency.ann.NeuralNetwork;
 import com.cluttered.cryptocurrency.ann.neuron.Neuron;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
@@ -20,7 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class XorTest {
 
-    private static final Logger LOG = LoggerFactory.getLogger(XorTest.class);
+    //private static final Logger LOG = LoggerFactory.getLogger(XorTest.class);
 
     private static BigDecimal bigDecimal(final String value) {
         return new BigDecimal(value);
@@ -55,25 +53,25 @@ public class XorTest {
         // [0, 0] -> [0]
         final List<BigDecimal> inputs1 = Arrays.asList(ZERO, ZERO);
         final BigDecimal result1 = neuralNetwork.fire(inputs1).get(0).setScale(10, ROUND_HALF_UP);
-        LOG.info("1) {} -> {}", inputs1, result1);
+        //LOG.info("1) {} -> {}", inputs1, result1);
         assertThat(result1).isEqualTo(ZERO.setScale(10));
 
         // [1, 0] -> [1]
         final List<BigDecimal> inputs2 = Arrays.asList(ONE, ZERO);
         final BigDecimal result2 = neuralNetwork.fire(inputs2).get(0).setScale(10, ROUND_HALF_UP);
-        LOG.info("2) {} -> {}", inputs2, result2);
+        //LOG.info("2) {} -> {}", inputs2, result2);
         assertThat(result2).isEqualTo(ONE.setScale(10));
 
         // [0, 1] -> [1]
         final List<BigDecimal> inputs3 = Arrays.asList(ZERO, ONE);
         final BigDecimal result3 = neuralNetwork.fire(inputs3).get(0).setScale(10, ROUND_HALF_UP);
-        LOG.info("3) {} -> {}", inputs3, result3);
+        //LOG.info("3) {} -> {}", inputs3, result3);
         assertThat(result3).isEqualTo(ONE.setScale(10));
 
         // [1, 1] -> [0]
         final List<BigDecimal> inputs4 = Arrays.asList(ONE, ONE);
         final BigDecimal result4 = neuralNetwork.fire(inputs4).get(0).setScale(10, ROUND_HALF_UP);
-        LOG.info("4) {} -> {}", inputs4, result4);
+        //LOG.info("4) {} -> {}", inputs4, result4);
         assertThat(result4).isEqualTo(ZERO.setScale(10));
     }
 }
