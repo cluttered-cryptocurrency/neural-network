d9abe4828f883b80899f1732f1b9debd608fb12e
David Clutter <cluttered.code@gmail.com>
2017-07-19 21:46:34 -0600

    more LayerTests

--- src/test/java/com/cluttered/cryptocurrency/ann/LayerTest.java
+++ src/test/java/com/cluttered/cryptocurrency/ann/LayerTest.java
@@ -9,11 +9,11 @@ import mockit.integration.junit4.JMockit;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import static com.cluttered.cryptocurrency.ann.activation.Activation.TAN_H;
+import static mockit.Deencapsulation.setField;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author cluttered.code@gmail.com
@@ -41,24 +41,20 @@ public class LayerTest {
         eligibleActivations.add(TAN_H);
 
         final Layer result = Layer.random(inputSize, size, eligibleActivations);
+
+        assertThat(result).isNotNull();
     }
 
     @Test
     public void testFire() {
-//        final List<Double> inputs = Arrays.asList(1.0, 2.0, 3.0);
-//        final Neuron neuron1 = Neuron.builder().weights(1.0, 1.0, 1.0).build();
-//        final Neuron neuron2 = Neuron.builder().weights(1.0, 1.0, 1.0).sigmoid().build();
-//        final Neuron neuron3 = Neuron.builder().weights(1.0, 1.0, 1.0).tanH().build();
-//        final List<Neuron> neurons = Arrays.asList(neuron1, neuron2, neuron3);
-//        setField(layer, "neurons", neurons);
-//
-//        final List<Double> result = layer.fire(inputs);
-//
-//        assertThat(result).containsExactly(
-//                6.0,
-//                0.9975273768433653,
-//                0.9999877116507956
-//        );
+        final List<Double> inputs = Arrays.asList(1.0, 2.0, 3.0);
+        final Neuron neuron = Neuron.random(inputs.size(), Collections.emptySet());
+        final List<Neuron> neurons = Arrays.asList(neuron, neuron, neuron);
+        setField(layer, "neurons", neurons);
+
+        final List<Double> result = layer.fire(inputs);
+
+        assertThat(result.size()).isEqualTo(3);
 
     }
 }
