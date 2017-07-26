61cd5fba1316ae25ef4673af1c054befe32cd275
David Clutter <cluttered.code@gmail.com>
2017-07-26 11:53:20 -0600

    Test population trainingAndSortGeneration

--- src/test/java/com/cluttered/cryptocurrency/ga/PopulationTest.java
+++ src/test/java/com/cluttered/cryptocurrency/ga/PopulationTest.java
@@ -1,12 +1,15 @@
 package com.cluttered.cryptocurrency.ga;
 
+import com.cluttered.cryptocurrency.ga.impl.TestChromosomeImpl;
 import com.cluttered.cryptocurrency.ga.impl.TestPopulationImpl;
 import mockit.Expectations;
 import mockit.Mocked;
 import mockit.Tested;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 public class PopulationTest {
 
@@ -30,4 +33,17 @@ public class PopulationTest {
         population.trainingLoop(inputs);
     }
 
+    @Test
+    public void testTrainAndSortGeneration(@Mocked final Collection<Double> inputs,
+                                           @Mocked final TestChromosomeImpl chromosome) {
+        final List<TestChromosomeImpl> generation = Arrays.asList(chromosome, chromosome, chromosome);
+
+        new Expectations(population) {{
+            population.getGeneration(); times = 1; result = generation;
+            //chromosome.train(inputs); times = generation.size();
+            population.setGeneration((List<TestChromosomeImpl>) any); times = 1;
+        }};
+
+        population.trainAndSortGeneration(inputs);
+    }
 }
